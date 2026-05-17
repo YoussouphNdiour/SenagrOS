0e8a6741094e51339ee3564dd6035b77c85eb7e4
@ndiouryoussouph <youssouph.ndiour@sen-gt.com>
2026-05-17 13:24:14 +0000

    fix: set InertiaRails layout to 'inertia' in correct initializer

--- config/initializers/inertia.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-# Configure inertia_rails to use the app's Inertia layout instead of the
-# default 'application' layout (which doesn't exist in this project).
-InertiaRails.configure do |config|
-  config.layout = 'inertia'
-end
